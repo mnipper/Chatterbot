048f05790a1f681e301d26a6f5ec0716e2a6afd6
Michael Nipper <mjn4406@gmail.com>
2012-10-17 19:01:50 -0400

    Add preprocessor and random response

--- chatterbot.rb
+++ chatterbot.rb
@@ -12,4 +12,31 @@ class ChatterBot
       raise "Can't load bot data"
     end
   end
+
+  def greeting
+    random_response :greeting
+  end
+
+  def farewell
+    random_response :farewell
+  end
+
+  def response_to(input)
+    prepared_input = preprocess(input).downcase
+  end
+
+  private
+
+  def random_response(key)
+    @data[:responses][key].sample.gsub(/\[name\]/, @name)
+  end
+
+  def preprocess(input)
+    perform_substitutions input
+  end
+
+  def perform_substitutions(input)
+    @data[:presubs].each { |s| input.gsub!(s[0], s[1]) }
+    input
+  end
 end
